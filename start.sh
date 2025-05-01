c78498192a92b49aa91df4a8c80d6b024a6a39c2
Atishay Patni <apatni@Atishays-MacBook-Air.local>
2025-05-02 03:21:42 +0530

    Solving the web socket error

--- start.sh
+++ start.sh
@@ -9,9 +9,5 @@ uvicorn summarization_service.main:app --host 0.0.0.0 --port 8082 --proxy-header
 # Start resolver service
 uvicorn podcast_audio_resolver_service.main:app --host 0.0.0.0 --port 8080 &
 
-# Wait to ensure all services are running before NGINX starts
-echo "⏳ Waiting 5 seconds for services to be ready..."
-sleep 5
-
 # Start NGINX last so it doesn’t race
 nginx -g 'daemon off;'
